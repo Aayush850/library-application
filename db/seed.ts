d8b7371184a9ce8979a10903592f7feb3a30dbdb
Aayush Paudel <aayushpaudel87@gmail.com>
2025-12-09 15:35:33 +0545

    fix: seeding

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,8 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import prisma from "@/db/prisma";
 import { sampleData } from "./sample-data";
 import "dotenv/config";
 
 async function seed() {
-  const prisma = new PrismaClient();
   await prisma.genre.deleteMany();
   await prisma.genre.createMany({ data: sampleData.genres });
   console.log("Database seeded successfully.");
